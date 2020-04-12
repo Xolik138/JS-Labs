acf79dd1a6b0319532ac8022d6772ec5eb787a4f
Xolik <sergey138569@yandex.ru>
2020-04-12 22:16:54 +0300

    v1

--- lab6/index.js
+++ lab6/index.js
@@ -1,12 +1,7 @@
-var server = require("./server");
-var router = require("./router");
-var requestHandlers = require("./requestHandlers");
-var handle = {}
-handle["/"] = requestHandlers.start;
-handle["/start"] = requestHandlers.start;
-handle["/upload"] = requestHandlers.upload;
-handle["/download"] = requestHandlers.download;
-handle["/giena"] = requestHandlers.giena;
-handle["/gazel"] = requestHandlers.gazel;
-handle["/gekkon"] = requestHandlers.gekkon;
-server.start(router.route, handle);
+const express = require("express");
+
+const app = express();
+
+app.listen(8000,()=>{
+  console.log("server is listening");
+})
