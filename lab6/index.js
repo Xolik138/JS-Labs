16f877d07785d5f628b03548295ffb601825fa75
Xolik <sergey138569@yandex.ru>
2020-04-17 17:36:20 +0300

    v1

--- lab6/index.js
+++ lab6/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
-
+var bodyParser = require('body-parser')
 const app = express();
 
+app.use(bodyParser.text());
 app.use("/",require("./router"));
 
 app.listen(8888,()=>{
--- lab6/router.js
+++ lab6/router.js
@@ -2,7 +2,9 @@ const express = require("express");
 var fs = require("fs");
 const router = express.Router();
 
-router.get("/",(req,res)=>{
+
+
+router.get("/", (req, res) => {
   console.log("GET /");
   var body = fs.readFileSync('six.html');
   res.writeHead(200, {
@@ -12,8 +14,30 @@ router.get("/",(req,res)=>{
   res.end();
 });
 
-router.post("/",(req,res)=>{
-  res.send({method:"POST"});
-  console.log("POST /")
+router.post("/gazel", (req, res) => {
+  console.log(req.body);
+  console.log("POST /gazel")
 })
+router.get("/gazel", (req, res) => {
+  console.log(req.body);
+  console.log("GET /gazel")
+})
+router.post("/gekkon", (req, res) => {
+  console.log(req.body);
+  console.log("POST /gekkon")
+})
+router.get("/gekkon", (req, res) => {
+  console.log(req.body);
+  console.log("GET /gekkon")
+})
+router.post("/giena", (req, res) => {
+  console.log(req.body);
+  console.log("POST /giena")
+})
+router.get("/giena", (req, res) => {
+  console.log(req.body);
+  console.log("GET /giena")
+})
+
+
 module.exports = router;
