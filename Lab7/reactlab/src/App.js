b7f8d8f15e63b42097c6de4324fc9824a2d5953b
Xolik <sergey138569@yandex.ru>
2020-04-29 16:16:19 +0300

    v1

--- Lab7/reactlab/src/App.js
+++ Lab7/reactlab/src/App.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import checkan from './checkan.js';
 
 function App() {
   return (
-    <div> </div>
+    <form >
+      <p><b>Название животного:</b><br />
+      <input name='namean' type='text'/></p>
+
+      <p><b>Номер:</b><br />
+      <input name='valan' type='text'/></p>
+
+      <p><b>Высота изоб.:</b><br />
+      <input name='han' type='text'/></p>
+
+      <p><b>Ширина изоб.:</b><br />
+      <input name='wan' type='text'/></p>
+
+      <p><b>Цвет изоб.:</b><br />
+      <input name='can' type='text'/></p>
+
+      <button classname = 'knopka' onClick={checkan}>Отпавить</button>
+    </form>
   );
 }
 
